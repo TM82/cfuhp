311c0571e5b4bd079e55919503672a4b897c7505
Takahiro Miura <ttmiura225@gmail.com>
2019-08-05 00:54:24 +0900

    fix

--- app/src/main.js
+++ app/src/main.js
@@ -10,16 +10,6 @@ Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
-// const config = {
-//     apiKey: 'AIzaSyAO7YMo5t7eJNlzQ1z9R97GULdxj3MVVCs',
-//     authDomain: 'tmijob-83bea.firebaseapp.com',
-//     databaseURL: 'https://tmijob-83bea.firebaseio.com',
-//     projectId: 'tmijob-83bea',
-//     storageBucket: 'tmijob-83bea.appspot.com',
-//     messagingSenderId: '681361491430'
-// }
-// firebase.initializeApp(config);
-
 new Vue({
   router,
   store,
---
+++ app/src/views/Event.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="event">
+    <HelloWorld msg="This is HomePage of Code for University !!"/>
+    <button class="btn btn-primary">test</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'event',
+  components: {
+    HelloWorld
+  }
+}
+</script>
